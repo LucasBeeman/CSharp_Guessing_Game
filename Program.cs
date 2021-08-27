537e42faa0edf5d48cb28c01bf5af6aac2a27bcc
Lucas.B <70167892+LucasBeeman@users.noreply.github.com>
2021-08-26 21:59:48 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RandomNumberGuesser
 {
@@ -7,40 +8,37 @@ namespace RandomNumberGuesser
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int size = 100;
+            List<int> guessList = new List<int>();
+            int size = 2147483646;
             int answer = rand.Next(size + 1);
             int guess = size / 2;
             int min = 0;
             int max = size;
             int miss = 0;
-
+            int guessNum = 0;
             Console.WriteLine("The answer is " + answer);
 
             while (guess != answer)
             {
                 if (guess > answer)
                 {
-                    max -= size - guess;
+                    max -= max - guess - 1;
                     miss++;
-                    if(max < min)
-                    {
-                        min = 0;
-                    }
                 }
                 else if (guess < answer)
                 {
-                    min = guess +1;
+                    min = guess + 1;
                     miss++;
                 }
-                guess = rand.Next(min, max + 1);
-                Console.WriteLine(guess);
-                Console.WriteLine(max);
-                Console.WriteLine(min);
-                Console.WriteLine("-------------");
-
+                guess = rand.Next(min, max);
+                guessList.Add(guess);
+                guessNum++;
+                Console.WriteLine(guessList[guessNum - 1]);
             }
             Console.WriteLine("\nThe correct number is " + answer);
             Console.WriteLine("\nThe bot has missed " + miss + " times");
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadLine();
         }
     }
 }
